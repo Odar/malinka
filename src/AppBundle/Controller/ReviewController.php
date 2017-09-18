23e224c1b726cf0962a84b61bf937a69b8940122
eavasenin <eavasenin@gmail.com>
2017-09-18 23:01:24 +0300

    создана страница reviews, assretType не работает

--- src/AppBundle/Controller/ReviewController.php
+++ src/AppBundle/Controller/ReviewController.php
@@ -2,6 +2,8 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Review;
+use AppBundle\Form\ReviewType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -9,10 +11,36 @@ use Symfony\Component\HttpFoundation\Request;
 class ReviewController extends Controller
 {
     /**
-     * @Route("/otzivi")
+     * @Route("/reviews")
      */
-    public function otziviAction()
+    public function ReviewsAction()
     {
-        return $this->render('AppBundle:Otzivi:otzivi.html.twig');
+        $em = $this->getDoctrine()->getManager();
+        $reviews = $em->getRepository(Review::class)->findAll();
+
+        return $this->render('AppBundle:Review:review.html.twig', [
+            'reviews' => $reviews
+        ]);
+    }
+
+    /**
+     * @Route("/review/add")
+     */
+    public function AddReviewAction(Request $request)
+    {
+        $review = new Review();
+        $form = $this->createForm(ReviewType::class, $review);
+        $form->handleRequest($request);
+
+        if ($form->isValid() && $form->isSubmitted()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($review);
+            $em->flush();
+            return $this->redirectToRoute('app_review_reviews');
+        }
+
+        return $this->render('AppBundle:Review:form.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 }
